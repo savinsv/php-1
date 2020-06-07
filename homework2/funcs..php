a85ae4a842d18d90607174b42523148e5996fbbd
Savin Sergey <razi@yandex.ru>
2020-06-07 23:24:28 +0300

    Добавлены новые файлы...

---
+++ homework2/funcs..php
@@ -0,0 +1,14 @@
+<?php
+    function sum(num1,num2){
+        return num1 + num2;
+    };
+
+    function difference(num1, num2){
+        return num1 - num2;
+    }; 
+
+    function product(num1,num2){
+        return num1 * num2;
+    };
+
+?>
