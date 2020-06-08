537f36229748e53022531b635f929eb4702af313
Savin Sergey <razi@yandex.ru>
2020-06-08 17:22:25 +0300

    Добавлен sablon.html, дораработан код решения нескольких заданий ДЗ. Файл funcs.php не используется.

--- homework2/funcs..php
+++ homework2/funcs..php
@@ -1,14 +1,22 @@
 <?php
-    function sum(num1,num2){
-        return num1 + num2;
+    function sum($num1, $num2){
+        return $num1 + $num2;
     };
 
-    function difference(num1, num2){
-        return num1 - num2;
+    function difference($num1, $num2){
+        return $num1 - $num2;
     }; 
 
-    function product(num1,num2){
-        return num1 * num2;
+    function product($num1,$num2){
+        return $num1 * $num2;
     };
 
+    function show_task1(){
+        $data ='<div class="firsttask">';
+        $data .= "Получаем случайное число от -20 до +20 и присваиваем переменной a. <br>";
+        $a = rand(-20,+20);
+        $data .= "a = ".$a ."<br>";
+        $data .= '</div>';
+        return $data;
+    }
 ?>
